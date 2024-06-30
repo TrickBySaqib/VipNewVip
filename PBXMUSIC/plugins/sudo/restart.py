084a5e9dc9fbb6f2855d59de5c2d58786c6444a8
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-06-30 16:45:29 +0530

    Update restart.py

--- PBXMUSIC/plugins/sudo/restart.py
+++ PBXMUSIC/plugins/sudo/restart.py
@@ -184,7 +184,7 @@ async def update_(client, message, _):
         exit()
 
 
-@app.on_message(filters.command(["restart"]) & SUDOERS)
+@app.on_message(filters.command("mrestart"))
 async def restart_(_, message):
     response = await message.reply_text("ʀᴇsᴛᴀʀᴛɪɴɢ...")
     ac_chats = await get_active_chats()
