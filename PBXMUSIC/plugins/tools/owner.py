87f5b6c2e619367a9d6132858ccfaa8c839743ab
Badmunda011 <Badmunda011@users.noreply.github.com>
2024-07-02 04:46:54 +0000

    Pylint: Auto Fixes

--- PBXMUSIC/plugins/tools/owner.py
+++ PBXMUSIC/plugins/tools/owner.py
@@ -4,52 +4,70 @@ from PBXMUSIC import app
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
-
-
-@app.on_message(filters.command(["owner"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.group)
+@app.on_message(
+    filters.command(["owner"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"])
+    & filters.group
+)
 async def help(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/1232e6dca7142e8ed175a.jpg",
         caption=f"""☠ ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ ᴄᴏɴᴛᴀᴄᴛ ᴏᴡɴᴇʀ 🌸""",
         reply_markup=InlineKeyboardMarkup(
             [
-[
-        InlineKeyboardButton(text=" ʙᴀᴅ 🌸", url=f"https://t.me/ll_BAD_MUNDA_ll"),
-        InlineKeyboardButton(text=" ᴍᴀɴɪ 🌸", url=f"https://t.me/ll_mxni_ll"),
-    ],
+                [
+                    InlineKeyboardButton(
+                        text=" ʙᴀᴅ 🌸", url=f"https://t.me/ll_BAD_MUNDA_ll"
+                    ),
+                    InlineKeyboardButton(
+                        text=" ᴍᴀɴɪ 🌸", url=f"https://t.me/ll_mxni_ll"
+                    ),
+                ],
             ]
         ),
     )
 
 
-@app.on_message(filters.command(["owner"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.group)
+@app.on_message(
+    filters.command(["owner"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"])
+    & filters.group
+)
 async def help(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/1232e6dca7142e8ed175a.jpg",
         caption=f"""☠ ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ ᴄᴏɴᴛᴀᴄᴛ ᴏᴡɴᴇʀ 🌸""",
         reply_markup=InlineKeyboardMarkup(
             [
-            [
-        InlineKeyboardButton(text=" ʙᴀᴅ 🌸", url=f"https://t.me/ll_BAD_MUNDA_ll"),
-        InlineKeyboardButton(text=" ᴍᴀɴɪ 🌸", url=f"https://t.me/ll_mxni_ll"),
-    ],
-
+                [
+                    InlineKeyboardButton(
+                        text=" ʙᴀᴅ 🌸", url=f"https://t.me/ll_BAD_MUNDA_ll"
+                    ),
+                    InlineKeyboardButton(
+                        text=" ᴍᴀɴɪ 🌸", url=f"https://t.me/ll_mxni_ll"
+                    ),
+                ],
             ]
         ),
     )
 
 
-@app.on_message(filters.command(["owner"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.group)
+@app.on_message(
+    filters.command(["owner"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"])
+    & filters.group
+)
 async def help(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/1232e6dca7142e8ed175a.jpg",
         caption=f"""☠ ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ ᴄᴏɴᴛᴀᴄᴛ ᴏᴡɴᴇʀ 🌸""",
         reply_markup=InlineKeyboardMarkup(
             [
-             [
-        InlineKeyboardButton(text=" ʙᴀᴅ 🌸", url=f"https://t.me/ll_BAD_MUNDA_ll"),
-        InlineKeyboardButton(text=" ᴍᴀɴɪ 🌸", url=f"https://t.me/ll_mxni_ll"),
-    ],
+                [
+                    InlineKeyboardButton(
+                        text=" ʙᴀᴅ 🌸", url=f"https://t.me/ll_BAD_MUNDA_ll"
+                    ),
+                    InlineKeyboardButton(
+                        text=" ᴍᴀɴɪ 🌸", url=f"https://t.me/ll_mxni_ll"
+                    ),
+                ],
             ]
         ),
     )
